0b92e92990797752d54d0e2e9677ee70f2f078ed
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2026-08-12 08:09:01 +0200

    Initial commit

---
+++ ModPublisher.cs
@@ -0,0 +1,232 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Core.Collections.Scoped;
+using Core.Localization;
+using Game.Core.Modding;
+using Game.Modding;
+using Game.Orchestration;
+using JetBrains.Annotations;
+using Menu.MainMenu.Mods;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using Shapez2UILib;
+using ShapezShifter.Kit;
+using ShapezShifter.SharpDetour;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements.Collections;
+
+namespace ModPublisher;
+
+public class ModPublisher : IMod
+{
+    public static Core.Logging.ILogger Logger;
+    // This list has to be hardcoded, because "someone" is still shipping a years old steamworks version with their game >:(
+    [ItemCanBeNull]
+    public static string[] GameBranches = [null, "public", "0.0.9-old", "0.1.1-old", "experimental", "modding_stable"];
+
+    public static ModFolderLocator Resources;
+    
+    private static Hook _modMenuConstructorHook;
+    
+    public ModPublisher(Core.Logging.ILogger logger)
+    {
+        Logger = logger;
+        Resources = ModDirectoryLocator.CreateLocator<ModPublisher>().SubLocator("Resources");
+        MainMenuUIRegistrar.RegisterUI<PrepareUploadMenuState>(
+            BuildPrepareUploadMenu,
+            "ModUpload",
+            "menu.prepare-upload.title".T(),
+            "menu.prepare-upload.title",
+            "there's no menu with this name, because I don't want this directly in the main menu",
+            "About"
+            );
+        _modMenuConstructorHook = DetourHelper
+            .CreatePostfixHook<HUDModMenuEntry, IModDescriptor, ModManifest, IDictionary<IModId, IModDescriptor>>(
+                (entry, descriptor, modManifest, mods) => entry.Construct(descriptor, modManifest, mods),
+                OnModEntryConstruct
+            );
+    }
+    public void Dispose()
+    {
+        _modMenuConstructorHook.Dispose();
+    }
+    public void BuildPrepareUploadMenu(PrepareUploadMenuState prepareUploadMenuState)
+    {
+        var MainContent = prepareUploadMenuState.transform.GetChild(0);
+        // Info Text
+        var infoText = UIFactory.AddLocalizedTextPrimary(MainContent, prepareUploadMenuState);
+        var infoTextRT = infoText.GetComponent<RectTransform>();
+        infoTextRT.anchorMin = Vector2.up;
+        infoTextRT.anchorMax = Vector2.up;
+        infoTextRT.offsetMin = new Vector2(20, -80);
+        infoTextRT.offsetMax = new Vector2(200, -20);
+        // Title
+        var titleText = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var titleTextRT = titleText.GetComponent<RectTransform>();
+        titleTextRT.anchorMin = Vector2.up;
+        titleTextRT.anchorMax = Vector2.one;
+        titleTextRT.offsetMin = new Vector2(20, -140);
+        titleTextRT.offsetMax = new Vector2(-20, -80);
+        titleText.Alignment = TextAlignmentOptions.Left;
+        // Preview
+        var previewLabel = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var previewLabelRT = previewLabel.GetComponent<RectTransform>();
+        previewLabelRT.anchorMin = Vector2.up;
+        previewLabelRT.anchorMax = Vector2.up;
+        previewLabelRT.offsetMin = new Vector2(-20, -80);
+        previewLabelRT.offsetMax = new Vector2(410, -20);
+        previewLabel.Alignment = TextAlignmentOptions.Right;
+        var openPreviewButton = UIFactory.AddButton(MainContent, prepareUploadMenuState, secondary: true);
+        var openPreviewButtonRT = openPreviewButton.GetComponent<RectTransform>();
+        openPreviewButtonRT.anchorMin = Vector2.up;
+        openPreviewButtonRT.anchorMax = Vector2.up;
+        openPreviewButtonRT.offsetMin = new Vector2(310, -120);
+        openPreviewButtonRT.offsetMax = new Vector2(410, -80);
+        var previewImage = UIFactory.AddSecondaryPanel(MainContent);
+        var previewImageRT = previewImage.GetComponent<RectTransform>();
+        previewImageRT.anchorMin = Vector2.up;
+        previewImageRT.anchorMax = Vector2.up;
+        previewImageRT.offsetMin = new Vector2(440, -280);
+        previewImageRT.offsetMax = new Vector2(880, -30);
+        // Workshop status
+        var workshopStatusText = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var workshopStatusRT = workshopStatusText.GetComponent<RectTransform>();
+        workshopStatusRT.anchorMin = Vector2.up;
+        workshopStatusRT.anchorMax = Vector2.up;
+        workshopStatusRT.offsetMin = new Vector2(20, -240);
+        workshopStatusRT.offsetMax = new Vector2(400, -140);
+        workshopStatusText.Alignment = TextAlignmentOptions.TopLeft;
+        workshopStatusText.UIText.textWrappingMode = TextWrappingModes.Normal;
+        // Description
+        var descriptionLabel = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var descriptionLabelRT = descriptionLabel.GetComponent<RectTransform>();
+        descriptionLabelRT.anchorMin = Vector2.up;
+        descriptionLabelRT.anchorMax = Vector2.one;
+        descriptionLabelRT.offsetMin = new Vector2(20, -300);
+        descriptionLabelRT.offsetMax = new Vector2(-20, -240);
+        descriptionLabel.Alignment = TextAlignmentOptions.Left;
+        var descriptionInput = UIFactory.AddInputField(MainContent, prepareUploadMenuState);
+        var inputComponent = descriptionInput.UIInputField;
+        inputComponent.lineType = TMP_InputField.LineType.MultiLineNewline;
+        inputComponent.lineLimit = 100;
+        inputComponent.characterLimit = 10000;
+        inputComponent.characterValidation = TMP_InputField.CharacterValidation.None;
+        inputComponent.textComponent.parseCtrlCharacters = false;
+        inputComponent.textComponent.verticalAlignment = VerticalAlignmentOptions.Top;
+        var descriptionInputRT = descriptionInput.GetComponent<RectTransform>();
+        descriptionInputRT.anchorMin = Vector2.up;
+        descriptionInputRT.anchorMax = Vector2.one;
+        descriptionInputRT.offsetMin = new Vector2(20, -540);
+        descriptionInputRT.offsetMax = new Vector2(-20, -300);
+        // Changelog
+        var changelogLabel = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var changelogLabelRT = changelogLabel.GetComponent<RectTransform>();
+        changelogLabelRT.anchorMin = Vector2.up;
+        changelogLabelRT.anchorMax = Vector2.one;
+        changelogLabelRT.offsetMin = new Vector2(20, -610);
+        changelogLabelRT.offsetMax = new Vector2(-20, -550);
+        changelogLabel.Alignment = TextAlignmentOptions.Left;
+        var changelogInput = UIFactory.AddInputField(MainContent, prepareUploadMenuState);
+        var changelogComponent = changelogInput.UIInputField;
+        changelogComponent.lineType = TMP_InputField.LineType.MultiLineNewline;
+        changelogComponent.lineLimit = 100;
+        changelogComponent.characterLimit = 10000;
+        changelogComponent.characterValidation = TMP_InputField.CharacterValidation.None;
+        changelogComponent.textComponent.parseCtrlCharacters = false;
+        changelogComponent.textComponent.verticalAlignment = VerticalAlignmentOptions.Top;
+        var changelogInputRT = changelogInput.GetComponent<RectTransform>();
+        changelogInputRT.anchorMin = Vector2.up;
+        changelogInputRT.anchorMax = Vector2.one;
+        changelogInputRT.offsetMin = new Vector2(20, -750);
+        changelogInputRT.offsetMax = new Vector2(-20, -610);
+        // Dependencies
+        var dependenciesLabel = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var dependenciesLabelRT = dependenciesLabel.GetComponent<RectTransform>();
+        dependenciesLabelRT.anchorMin = Vector2.up;
+        dependenciesLabelRT.anchorMax = Vector2.one;
+        dependenciesLabelRT.offsetMin = new Vector2(90, -810);
+        dependenciesLabelRT.offsetMax = new Vector2(-20, -750);
+        dependenciesLabel.Alignment = TextAlignmentOptions.Left;
+        var dependenciesToggle = UIFactory.AddToggle(MainContent, prepareUploadMenuState);
+        var dependenciesToggleRT = dependenciesToggle.GetComponent<RectTransform>();
+        dependenciesToggleRT.anchorMin = Vector2.up;
+        dependenciesToggleRT.anchorMax = Vector2.up;
+        dependenciesToggleRT.offsetMin = new Vector2(20, -810);
+        dependenciesToggleRT.offsetMax = new Vector2(80, -750);
+        // Version range
+        var versionLabel = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var versionLabelRT = versionLabel.GetComponent<RectTransform>();
+        versionLabelRT.anchorMin = Vector2.up;
+        versionLabelRT.anchorMax = Vector2.one;
+        versionLabelRT.offsetMin = new Vector2(20, -870);
+        versionLabelRT.offsetMax = new Vector2(-20, -810);
+        versionLabel.Alignment = TextAlignmentOptions.Left;
+        var versionToLabel = UIFactory.AddLocalizedTextSecondary(MainContent, prepareUploadMenuState);
+        var versionToLabelRT = versionToLabel.GetComponent<RectTransform>();
+        versionToLabelRT.anchorMin = new Vector2(0.45f, 1);
+        versionToLabelRT.anchorMax = new Vector2(0.45f, 1);
+        versionToLabelRT.offsetMin = new Vector2(-50, -870);
+        versionToLabelRT.offsetMax = new Vector2(50, -810);
+        var versionFromDropdown = UIFactory.AddDropdown(MainContent, prepareUploadMenuState);
+        var versionFromDropdownRT = versionFromDropdown.GetComponent<RectTransform>();
+        versionFromDropdownRT.anchorMin = new Vector2(0.45f, 1);
+        versionFromDropdownRT.anchorMax = new Vector2(0.45f, 1);
+        versionFromDropdownRT.offsetMin = new Vector2(-180, -870);
+        versionFromDropdownRT.offsetMax = new Vector2(-20, -810);
+        var versionToDropdown = UIFactory.AddDropdown(MainContent, prepareUploadMenuState);
+        var versionToDropdownRT = versionToDropdown.GetComponent<RectTransform>();
+        versionToDropdownRT.anchorMin = new Vector2(0.45f, 1);
+        versionToDropdownRT.anchorMax = new Vector2(0.45f, 1);
+        versionToDropdownRT.offsetMin = new Vector2(20, -870);
+        versionToDropdownRT.offsetMax = new Vector2(180, -810);
+        
+        var uploadButton = UIFactory.AddButton(MainContent, prepareUploadMenuState);
+        var uploadButtonRT = uploadButton.GetComponent<RectTransform>();
+        uploadButtonRT.anchorMin = Vector2.zero;
+        uploadButtonRT.anchorMax = Vector2.zero;
+        uploadButtonRT.offsetMin = new Vector2(20, 20);
+        uploadButtonRT.offsetMax = new Vector2(420, 80);
+        
+        
+        prepareUploadMenuState.infoText = infoText;
+        prepareUploadMenuState.titleText = titleText;
+        prepareUploadMenuState.previewLabel = previewLabel;
+        prepareUploadMenuState.openPreviewButton = openPreviewButton;
+        prepareUploadMenuState.previewImage = previewImage.transform.Find("Panel").GetComponent<Image>();
+        prepareUploadMenuState.workshopStatusText = workshopStatusText;
+        prepareUploadMenuState.descriptionLabel = descriptionLabel;
+        prepareUploadMenuState.descriptionInput = descriptionInput;
+        prepareUploadMenuState.changelogLabel = changelogLabel;
+        prepareUploadMenuState.changelogInput = changelogInput;
+        prepareUploadMenuState.dependenciesLabel = dependenciesLabel;
+        prepareUploadMenuState.dependenciesToggle = dependenciesToggle;
+        prepareUploadMenuState.versionLabel = versionLabel;
+        prepareUploadMenuState.versionToLabel = versionToLabel;
+        prepareUploadMenuState.versionFromDropdown = versionFromDropdown;
+        prepareUploadMenuState.versionToDropdown = versionToDropdown;
+        prepareUploadMenuState.uploadButton = uploadButton;
+    }
+    public void OnModEntryConstruct(HUDModMenuEntry modEntry, IModDescriptor descriptor, ModManifest manifest, IDictionary<IModId, IModDescriptor> mods)
+    {
+        // Only allow local mods
+        // This can't be done by checking the type because a mod can also be OverridenModDescriptor
+        if (new DirectoryInfo(descriptor.DirectoryPath).Name != descriptor.ModTitle)
+            return;
+        var button = UIFactory.AddButton(modEntry.transform, modEntry, true);
+        var buttonRT = button.GetComponent<RectTransform>();
+        buttonRT.anchorMin = Vector2.zero;
+        buttonRT.anchorMax = Vector2.zero;
+        buttonRT.offsetMin = new Vector2(20, 10);
+        buttonRT.offsetMax = new Vector2(350, 50);
+        button.Text = "modpublisher.prepare-upload".T();
+        button.OnClick.AddListener(() =>
+        {
+            (GameBootstrapper.GameOrchestrator.CurrentSubOrchestrator as MainMenuOrchestrator)
+                ?.SwitchToState<PrepareUploadMenuState>(new ResolvedMod(manifest, descriptor));
+        });
+    }
+}
---
+++ PrepareUploadMenuState.cs
@@ -0,0 +1,151 @@
+﻿using System.Linq;
+using Core.Collections.Scoped;
+using Core.Dependency;
+using Core.Localization;
+using Game.Core.Modding;
+using Game.Modding;
+using Game.Modding.Steam;
+using JetBrains.Annotations;
+using Menu.MainMenu.Mods;
+using Shapez2UILib;
+using ShapezShifter.Textures;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModPublisher;
+
+public class PrepareUploadMenuState : HUDMainMenuState
+{
+    private readonly Sprite _previewPlaceholder;
+
+    public PrepareUploadMenuState()
+    {
+        var texture = FileTextureLoader.LoadTexture(ModPublisher.Resources.SubPath("preview_placeholder.png"));
+        RoundCorners(texture);
+        _previewPlaceholder = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height),
+            new Vector2(0.5f, 0.5f));
+    }
+    
+    [Construct]
+    private void Construct()
+    {
+        uploadButton.OnClick.AddListener(() => ModPublisher.Logger.Info.Log("Example Button Pressed"));
+        infoText.Text = "menu.prepare-upload.mod-info".T();
+        previewLabel.Text = "menu.prepare-upload.mod-preview".T();
+        previewImage.type = Image.Type.Simple;
+        previewImage.material = null;
+        openPreviewButton.Text = "menu.prepare-upload.open-preview".T();
+        descriptionLabel.Text = "menu.prepare-upload.mod-description".T();
+        changelogLabel.Text = "menu.prepare-upload.mod-changelog".T();
+        versionLabel.Text = "menu.prepare-upload.version-range".T();
+        versionToLabel.Text = "menu.prepare-upload.version-to".T();
+        versionToDropdown.Options = versionFromDropdown.Options =
+            ModPublisher.GameBranches.Select(FormatBranch).ToList();
+        uploadButton.Text = "menu.prepare-upload.upload-confirm".T();
+    }
+
+    private static IText FormatBranch([CanBeNull] string branch)
+    {
+        return branch switch
+        {
+            null => new RawText("any"),
+            "public" => new RawText("Latest Version"),
+            _ => new RawText(branch)
+        };
+    }
+
+    public override void OnDispose()
+    {
+            
+    }
+    public override void GoBack()
+    {
+        Menu.SwitchToState<HUDMenuModsState>();
+    }
+
+    public override void OnMenuEnterState(object payload)
+    {
+        if (payload is not ResolvedMod mod)
+        {
+            ModPublisher.Logger.Info?.Log("OnMenuEnterState payload was not resolved mod");
+            GoBack();
+            return;
+        }
+        
+        titleText.Text = "menu.prepare-upload.mod-title".T().Bind("mod-title", new RawText(mod.Descriptor.ModTitle));
+        
+        workshopStatusText.Color = Color.yellowNice;
+        workshopStatusText.Text = "menu.prepare-upload.title-is-new".T();
+
+        ModPublisher.Logger.Info!.LogFormat("Image: {0}", _previewPlaceholder);
+        previewImage.sprite = _previewPlaceholder;
+        
+        descriptionInput.Value = mod.Metadata.Description;
+        descriptionInput.UIInputField.caretPosition = 0;
+        descriptionInput.UIInputField.textComponent.rectTransform.anchoredPosition = Vector2.zero;
+        descriptionInput.UIInputField.AssignPositioningIfNeeded();
+        
+        changelogInput.Value = "v" + mod.Metadata.Version;
+        changelogInput.UIInputField.caretPosition = 0;
+        changelogInput.UIInputField.textComponent.rectTransform.anchoredPosition = Vector2.zero;
+        changelogInput.UIInputField.AssignPositioningIfNeeded();
+        
+        dependenciesLabel.Text = "menu.prepare-upload.toggle-dependencies".T().Bind("dependencies", FormatDependencies(mod.Metadata.Dependencies));
+        dependenciesToggle.Value = true;
+
+        versionToDropdown.Value = versionFromDropdown.Value = 0;
+    }
+
+    private IText FormatDependencies(VersionedModReference[] dependencies)
+    {
+        var steamDeps = dependencies.Where(dep => dep.ModId is SteamModId).ToList(); 
+        var contents = ScopedList.Get<IText>();
+        for(int i = 0; i < steamDeps.Count; i++)
+        {
+            if (i != 0)
+                contents.Add(i == steamDeps.Count - 1 ? new RawText(" and ") : new RawText(", "));
+            contents.Add(new RawText(steamDeps[i].ModTitle));
+        }
+        return new CombinedText(contents.ToArray());
+    }
+
+    private void RoundCorners(Texture2D texture)
+    {
+        var radius = Mathf.Min(texture.width, texture.height) / 5;
+        ModPublisher.Logger.Info!.LogFormat("Radius: {0}", radius);
+        for (int y = 0; y <= radius; y++)
+        {
+            for (int x = 0; x <= radius; x++)
+            {
+                var isVisible = x * x + y * y <= radius * radius;
+                if (isVisible)
+                    continue;
+                ModPublisher.Logger.Info!.LogFormat("Clearing: {0} {1}", radius - x, radius - y);
+                texture.SetPixel(radius - x, radius - y, new Color(0, 0, 0, 0));
+                texture.SetPixel(texture.width - 1 - radius + x, radius - y, new Color(0, 0, 0, 0));
+                texture.SetPixel(texture.width - 1 - radius + x, texture.height - 1 - radius + y, new Color(0, 0, 0, 0));
+                texture.SetPixel(radius - x, texture.height - 1 - radius + y, new Color(0, 0, 0, 0));
+            }
+        }
+        texture.Apply();
+    }
+
+
+    public HUDLocalizedText infoText;
+    public HUDLocalizedText titleText;
+    public HUDLocalizedText workshopStatusText;
+    public HUDLocalizedText previewLabel;
+    public HUDButton openPreviewButton;
+    public Image previewImage;
+    public HUDLocalizedText descriptionLabel;
+    public HUDInputField descriptionInput;
+    public HUDLocalizedText changelogLabel;
+    public HUDInputField changelogInput;
+    public HUDLocalizedText dependenciesLabel;
+    public HUDToggleControl dependenciesToggle;
+    public HUDLocalizedText versionLabel;
+    public HUDLocalizedText versionToLabel;
+    public HUDDropdownControl versionFromDropdown;
+    public HUDDropdownControl versionToDropdown;
+    public HUDButton uploadButton;
+}
